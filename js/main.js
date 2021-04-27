94dbf52661e5f3ff52d5cdca73b8ece6b1192da0
CanardKOB <83011277+KatieOB1999@users.noreply.github.com>
2021-04-27 10:46:10 -0400

    final-update

--- js/main.js
+++ js/main.js
@@ -16,26 +16,63 @@ for (i = 0; i < coll.length; i++) {
 /*SPECIAL EXHIBIT COLLAPSIBLES*/
 
 /*GENERAL ADMITTANCE COUNTERS*/
-let counterDisplayElem = document.querySelector('.counter-display');
-let counterMinusElem = document.querySelector('.counter-minus');
-let counterPlusElem = document.querySelector('.counter-plus');
 
-let count = 0;
+const container = document.querySelectorAll('.counter-container');
+const limit = 5;
 
-updateDisplay();
-
-counterPlusElem.addEventListener("click",()=>{
-    count++;
-    updateDisplay();
-}) ;
-
-counterMinusElem.addEventListener("click",()=>{
-    count--;
-    updateDisplay();
-});
-
-function updateDisplay(){
-    counterDisplayElem.innerHTML = count;
-};
+container.forEach(function(c){
+  const bm = c.querySelector('.counter-minus');
+  const bp = c.querySelector('.counter-plus');
+  const d = c.querySelector('.counter-display');
+  let count = 0;
+  bm.addEventListener('click',function(){
+    if(count>0){
+      count--;
+      displayAndCheck();
+    }
+  })
+  bp.addEventListener('click',function(){
+    if(count<limit){
+      count++;
+      displayAndCheck();
+    }
+  })
+  function displayAndCheck(){
+    d.textContent = count;
+    bm.disabled = count==0 ? true : false;
+    bp.disabled = count<limit ? false : true;
+  }
+})
 
 /*GENERAL ADMITTANCE COUNTERS*/
+
+
+/*POPUP*/
+
+var modal = document.getElementById("cart-confirm-popup");
+
+var btn = document.getElementById("confirm-cart");
+
+var span = document.getElementsByClassName("close-popup")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/*POPUP*/
+
+/*POPUP ON CART*/
+
+
+
+/*POPUP ON CART*/
